707c47d4d1cd818cea4476f800611caf2367af6a
Frank <101807917+frankischilling@users.noreply.github.com>
2025-11-04 17:29:37 -0500

    Update File Headers with Attribution and License

---
+++ fceux/ppu.h
@@ -0,0 +1,69 @@
+/* FCE Ultra - NES/Famicom Emulator
+ *
+ * Enhanced for fce360-enhanced
+ * GitHub: https://github.com/frankischilling/fce360-enhanced
+ * 
+ * Contributors:
+ * @frankischilling
+ * Ced2911 (original Xbox 360 port)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+void FCEUPPU_Init(void);
+void FCEUPPU_Reset(void);
+void FCEUPPU_Power(void);
+int FCEUPPU_Loop(int skip);
+
+void FCEUPPU_LineUpdate();
+void FCEUPPU_SetVideoSystem(int w);
+
+extern void (*PPU_hook)(uint32 A);
+extern void (*GameHBIRQHook)(void), (*GameHBIRQHook2)(void);
+
+/* For cart.c and banksw.h, mostly */
+extern uint8 NTARAM[0x800],*vnapage[4];
+extern uint8 PPUNTARAM;
+extern uint8 PPUCHRRAM;
+
+void FCEUPPU_SaveState(void);
+void FCEUPPU_LoadState(int version);
+uint8* FCEUPPU_GetCHR(uint32 vadr, uint32 refreshaddr);
+void ppu_getScroll(int &xpos, int &ypos);
+
+
+/* Avoid collision with XDK's FASTCALL in <xbox/winnt.h> */
+#ifndef FASTCALL
+#ifdef _MSC_VER
+#define FASTCALL __fastcall
+#else
+#define FASTCALL
+#endif
+#endif
+
+void PPU_ResetHooks();
+extern uint8 (FASTCALL *FFCEUX_PPURead)(uint32 A);
+extern void (*FFCEUX_PPUWrite)(uint32 A, uint8 V);
+extern uint8 FASTCALL FFCEUX_PPURead_Default(uint32 A);
+void FFCEUX_PPUWrite_Default(uint32 A, uint8 V);
+
+extern int scanline;
+extern uint8 PPU[4];
+
+enum PPUPHASE {
+	PPUPHASE_VBL, PPUPHASE_BG, PPUPHASE_OBJ
+};
+
+extern PPUPHASE ppuphase;
